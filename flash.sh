ea9dcc77ce8132f4681f70b4e63a28bec8fa0c4c
Kevin Chen <kevinchen2003@gmail.com>
2012-08-01 17:02:45 -0700

    Separate flashing into its own shell script for modularity.
    This lets you call flash multiple IMUs with the same code without recompiling or
    copying long commands.

---
+++ flash.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "** PRESS AND HOLD RESET"
+sleep 2
+echo "** RELEASE RESET"
+avrdude -p atmega328p -b 57600 -P /dev/tty.usbserial-A900fwgw -c stk500v1 -U flash:w:main.hex -U eeprom:w:main.eep -C /usr/local/CrossPack-AVR-20100115/etc/avrdude.conf
+screen /dev/tty.usbserial-A900fwgw 57600
